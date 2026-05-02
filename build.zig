970f18159203932f1124dd6975e99fb8c7e0461d
eero <songmqq@proton.me>
2026-05-02 13:02:22 +0800

    build: fix option declaration reuse

--- build.zig
+++ build.zig
@@ -3,6 +3,8 @@ const std = @import("std");
 pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
+    
+    const enable_krun = b.option(bool, "enable-krun", "Enable libkrun support") orelse false;
 
     const core_mod = b.createModule(.{
         .root_source_file = b.path("src/blink.zig"),
@@ -16,9 +18,7 @@ pub fn build(b: *std.Build) void {
         .root_module = core_mod,
     });
     lib.linkLibC();
-    // Only link krun if we are specifically targeting krun backend or it's available
-    // For now, we allow building without it for simulation purposes
-    if (b.option(bool, "enable-krun", "Enable libkrun support") orelse false) {
+    if (enable_krun) {
         lib.linkSystemLibrary("krun");
     }
     b.installArtifact(lib);
@@ -34,7 +34,7 @@ pub fn build(b: *std.Build) void {
         .root_module = exe_mod,
     });
     exe.linkLibC();
-    if (b.option(bool, "enable-krun", "Enable libkrun support") orelse false) {
+    if (enable_krun) {
         exe.linkSystemLibrary("krun");
     }
     b.installArtifact(exe);
